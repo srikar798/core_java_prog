c3c36786046ab081c1893d612578bb7d1d29d438
srikar798 <104735629+srikar798@users.noreply.github.com>
2023-09-22 19:19:27 -0700

    added while loop example

---
+++ src/main/java/com/meta/cj/day6/FactorialOfNumbers.java
@@ -0,0 +1,7 @@
+package com.meta.cj.day6;
+
+public class FactorialOfNumbers {
+    public static void main(String[] args) {
+        ;
+    }
+}
---
+++ src/main/java/com/meta/cj/day6/ReverseOfGivenNumber.java
@@ -0,0 +1,23 @@
+package com.meta.cj.day6;
+
+import java.util.Scanner;
+
+public class ReverseOfGivenNumber {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the number ");
+        int num = sc.nextInt();
+        int n = reverse(num);
+        System.out.println(" The reverse of " + num + " is " + n );
+    }
+    public static int reverse(int num ){
+        int rev = 0;
+        while( num != 0){
+            int r = num % 10;
+            rev = ( rev * 10 ) + r ;
+            num /= 10;
+        }
+        return(rev);
+    }
+
+}
