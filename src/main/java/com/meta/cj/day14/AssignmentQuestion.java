4f5d597d7251e99524e3e7cabfe8607c10c37387
srikar798 <104735629+srikar798@users.noreply.github.com>
2023-10-08 00:53:08 -0700

    added AssignmentQuestion answers

--- src/main/java/com/meta/cj/day14/AssignmentQuestion.java
+++ src/main/java/com/meta/cj/day14/AssignmentQuestion.java
@@ -2,6 +2,7 @@ package com.meta.cj.day14;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
 
 class Employees{
 
@@ -39,80 +40,99 @@ class Employees{
         return mobile;
     }
     public void showDetails(){
-        System.out.println("Department  = "+department);
-        System.out.println("EmpId  = "+empId);
-        System.out.println("Ename  = "+eName);
-        System.out.println("Salary = "+salary);
-        System.out.println("Mobile    = "+mobile);
+        System.out.print("  Dept   = "+department);
+        System.out.print("  EmpId  = "+empId);
+        System.out.print("  Salary = "+salary);
+        System.out.print("  Mobile = "+mobile);
+        System.out.print("  Ename  = "+eName);
     }
 }
 public class AssignmentQuestion {
-    public static List<Employees> employeeList = new ArrayList<>();
+
+    private static List<Employees> employeeList = new ArrayList<>();
+
     public static void main(String[] args) {
 
 
-        String data = "CS-1001-Manoj-55000-9876543210," +
-                "CS-1002-Rajesh-45000-9876543211," +
-                "CS-1003-Suresh-65000-9876543212," +
-                "CS-1004-Ramesh-75000-9876543213," +
-                "IS-1005-Suresh-55000-9876543214," +
-                "EC-1006-Ramesh-45000-9876543215," +
-                "IS-1007-Manoj-65000-9876543216," +
-                "EC-1008-Ramesh-75000-9876543217," +
-                "EC-1009-Jayesh-55000-9876543218," +
-                "EC-1010-Rani-45000-9876543219";
+            String data = "CS-1001-Manoj-55000-9876543210," +
+                    "CS-1002-Rajesh-45000-9876543211," +
+                    "CS-1003-Suresh-65000-9876543212," +
+                    "CS-1004-Ramesh-75000-9876543213," +
+                    "IS-1005-Suresh-55000-9876543214," +
+                    "EC-1006-Ramesh-45000-9876543215," +
+                    "IS-1007-Manoj-65000-9876543216," +
+                    "EC-1008-Ramesh-75000-9876543217," +
+                    "EC-1009-Jayesh-55000-9876543218," +
+                    "EC-1010-Rani-45000-9876543219";
 
-        String[] arr = data.split(",");
+            String[] arr = data.split(",");
 
 
-        for(int i=0; i< arr.length;i++){
-            String str = arr[i];
-            String[] list = str.split("-");
-            String department = list[0];
-            int empId = Integer.parseInt(list[1]);
-            String name = list[2];
-            double salary = Double.parseDouble(list[3]);
-            String mobile = list[4];
+            for (int i = 0; i < arr.length; i++) {
+                String str = arr[i];
+                String[] list = str.split("-");
+                String department = list[0];
+                int empId = Integer.parseInt(list[1]);
+                String name = list[2];
+                double salary = Double.parseDouble(list[3]);
+                String mobile = list[4];
 
-            Employees emp = new Employees(department,empId,name,salary,mobile);
-            employeeList.add(emp);
-        }
-
-        List<String> emp = getDepartmentNames();
-        for(String employee : emp){
-            System.out.println(employee);
-        }
-
-        List<String> mobileNumbers = getMobileOfEmployees();
-        for(String mobiles : mobileNumbers){
-            System.out.println(mobiles);
-        }
-
-        for(Employees employe : employeeList){
-            if(employe.getDepartment().equals("EC")){
-                employe.showDetails();
+                Employees emp = new Employees(department, empId, name, salary, mobile);
+                employeeList.add(emp);
             }
-        }
+            Scanner sc = new Scanner(System.in);
+            while(true) {
+                System.out.println("""
+                        1. Get All Department Names
+                        2. Get ALl Mobile Numbers of Employee
+                        3. Get Employees details of EC Department
+                        4. Exit
+                        Enter your choice(1-4):""");
+                int N = sc.nextInt();
 
+                System.out.println("-".repeat(30));
+                switch (N) {
+
+                    case 1 -> getDepartmentNames();
+                    case 2 -> getMobileOfEmployees();
+                    case 3 -> getEcDepartmentEmplyees();
+                    case 4 -> System.exit(0);
+                    default -> System.out.println("Invalid choice");
+
+                }
+                System.out.println("-".repeat(30));
+            }
 
     }
 
-    public static List<String>  getDepartmentNames(){
+    private static void getEcDepartmentEmplyees(){
+        for(Employees employe : employeeList){
+            if(employe.getDepartment().equals("EC")){
+                employe.showDetails();
+                System.out.println("");
+            }
+
+        }
+    }
+    private static void  getDepartmentNames(){
         List<String> departmentNames = new ArrayList<>();
         for(Employees emp : employeeList){
             if(!departmentNames.contains(emp.getDepartment())){
                 departmentNames.add(emp.getDepartment());
             }
         }
-
-        return departmentNames;
+        for(String employee : departmentNames){
+            System.out.println(employee);
+        }
     }
 
-    public static List<String> getMobileOfEmployees() {
+    private static void getMobileOfEmployees() {
         List<String> mobileNumbers = new ArrayList<>();
         for (Employees emp : employeeList) {
             mobileNumbers.add(emp.getMobile());
         }
-        return mobileNumbers;
+        for(String mobiles : mobileNumbers){
+            System.out.println(mobiles);
+        }
     }
 }
