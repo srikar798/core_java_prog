57b87fad284af2d66471e0c6d21f1c641077d41b
srikar798 <104735629+srikar798@users.noreply.github.com>
2023-10-10 18:50:39 -0700

    added day 18

---
+++ src/main/java/com/meta/cj/day18/MethodExamples.java
@@ -0,0 +1,17 @@
+package com.meta.cj.day18;
+
+import java.util.List;
+
+public class MethodExamples {
+
+    public List<String> getUniqueNames(List<String> names){
+        return null;
+    }
+
+    public static void main(String[] args) {
+
+        List<String> names = List.of("Krish","Manoj","Sai","Tanvi","Srikar","Sai","Krish");
+        MethodExamples obj = new MethodExamples();
+
+    }
+}
