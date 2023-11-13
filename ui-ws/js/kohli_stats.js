8d9e3c6f5ab9c5cce4e0ecb6b0439d81f3e9160a
srikar798 <104735629+srikar798@users.noreply.github.com>
2023-11-13 12:38:24 -0800

    added wi

--- ui-ws/js/kohli_stats.js
+++ ui-ws/js/kohli_stats.js
@@ -1,4 +1,10 @@
 data = null;
-window.onload = function(){fetch('../data/kohli_data.json').then(ele=>ele.json).then(ele => {data=ele;});}
+window.onload = function(){ 
+                            fetch('../data/kohli_data.json')
+                            .then(ele=>ele.json)
+                            .then(ele => {
+                                        data=ele;
+                                    });
+                                }
 
 console.log(data);
