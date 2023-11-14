92c54bba289f4505c3aeecf8ac2963389ed98843
srikar798 <104735629+srikar798@users.noreply.github.com>
2023-11-13 16:36:54 -0800

    added wi

--- ui-ws/js/kohli_stats.js
+++ ui-ws/js/kohli_stats.js
@@ -1,10 +1,92 @@
-data = null;
-window.onload = function(){ 
-                            fetch('../data/kohli_data.json')
-                            .then(ele=>ele.json)
-                            .then(ele => {
-                                        data=ele;
-                                    });
-                                }
 
-console.log(data);
+google.charts.load('current',{'packages':['corechart']});
+async function loadData() {
+    const res = await fetch('./data/kohli_data.json');
+    const jsondata = await res.json();
+    return jsondata;
+}
+
+function drawFormatWith100sChart(list_100_format){
+    let data = google.visualization.arrayToDataTable(list_100_format);
+    let options = {
+        title: 'Kohli 100s in Different Formats',
+        height: 300,
+        width: 400,
+    };
+    let chart = new google.visualization.ColumnChart(document.getElementById('idFormatWith100sChart'));
+    chart.draw(data,options);
+}
+
+window.onload = function(){
+    loadData().then(jsondata => {
+        arr = jsondata;
+        let format_100_map = new Map();
+        let country = new Set();
+        let format = new Set();
+        arr.forEach(ele => {
+            country.add(ele["Against"]);
+            format.add(ele["Format"]);
+            if(format_100_map[ele["Format"]]){
+                format_100_map[ele["Format"]] += 1;
+            }else{
+                format_100_map[ele["Format"]] = 1;
+            }
+        });
+    
+        let list_100_format = [["Format", "100 Count"]];
+        for(let key in format_100_map){
+            list_100_format.push([key,format_100_map[key]]);
+        }
+    
+        drawFormatWith100sChart(list_100_format);
+        showCountryDropDown(country);
+        document.getElementById("idCountry").addEventListener("change",function(){
+            showCountryDropDown(this.ariaValueMax,arr);
+        });
+        
+    
+    });
+}
+
+function showCountry100Information(country, arr, format){
+    let country_100_map = new Map();
+    arr.forEach(ele=> {
+        if(ele["Against"]== country){
+            if(country_100_map[ele["Format"]]){
+                country_100_map[ele["Format"]] += 1;
+            }else{
+                country_100_map[ele["Format"]] =1;
+            }
+        }
+    });
+    format.forEach(ele => {
+        if(!country_100_map[ele]){
+            country_100_map[ele] = 0;
+        }
+    });
+    let table100Counts = `<table class="table table-bordered table-stripped">
+        <thead>
+            <tr>
+                <th>Format</th>
+                <th>100s Count</th>
+            </tr>
+        </thead>
+        </tbody>`;
+    for(let key in country_100_map){
+        table100Counts += `<tr>
+            <td>${key}</td>
+            <td>${country_100_map[key]}</td>
+        </tr>`;
+    }
+    table100Counts += `</tbody></table>`;
+    document.getElementById("id100CountInfo").innerHTML = `<h2>${country}</h2>`;
+}
+function showCountryDropDown(country){
+    let str = `<select id="idCountry" class="form-select">`;
+    country.forEach(ele=>{
+        str += `<option value=${ele}>${ele}</option>`;
+    });
+    str += `</select`;
+    document.getElementById("idCountryDropDown").innerHTML = str;
+}
+
