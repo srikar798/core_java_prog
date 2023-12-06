e8c6cab4d8022ebf34a406e2eba2f97f6ce25f8d
srikar798 <104735629+srikar798@users.noreply.github.com>
2023-12-05 18:43:58 -0800

    added flyDb migration

--- spring-core/spring-core-tutorial/src/main/java/com/career/springcore/cbook/dao/ContactDaoImpl.java
+++ spring-core/spring-core-tutorial/src/main/java/com/career/springcore/cbook/dao/ContactDaoImpl.java
@@ -2,6 +2,9 @@ package com.career.springcore.cbook.dao;
 
 import com.career.springcore.cbook.domain.Contact;
 import lombok.RequiredArgsConstructor;
+import org.springframework.jdbc.support.GeneratedKeyHolder;
+import org.springframework.jdbc.support.KeyHolder;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -9,9 +12,23 @@ import java.util.List;
 @Repository
 @RequiredArgsConstructor
 public class ContactDaoImpl implements ContactDao{
+
+    private final JdbcTemplate jdbcTemplate;
+
+    private static final String INSERT_CONTACT = "insert into contact(name, email,mobile) values(?,?,?)";
     @Override
     public Contact insertContact(Contact contact) {
-        return null;
+        KeyHolder keyHolder = new GeneratedKeyHolder();
+        jdbcTemplate.update(connection -> {
+            var ps = connection.prepareStatement(INSERT_CONTACT,new String[]{"id"});
+            ps.setString(1,contact.getName());
+            ps.setString(1,contact.getEmail());
+            ps.setString(1,contact.getMobile());
+            return ps;
+        },keyHolder);
+        long id = keyHolder.getKey().longValue();
+        contact.setId(id);
+        return contact;
     }
 
     @Override
